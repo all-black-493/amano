681a6180e123ec19fc41e4693379e99efb35fe50
Jeremy Nyangi <justinjerome401@gmail.com>
2025-12-09 15:54:48 +0300

    fix: base url

--- lib/eden.ts
+++ lib/eden.ts
@@ -1,6 +1,11 @@
 import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
+const base = process.env.NEXT_PUBLIC_BASE_URL!
+
+if (!base) {
+  throw new Error('NEXT_PUBLIC_BASE_URL is not defined')
+}
 // this require .api to enter /api prefix
-export const client = treaty<App>('localhost:3000').api
+export const client = treaty<App>(base).api
 
